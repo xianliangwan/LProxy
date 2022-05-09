6fead69af0da928689ab2d541f0dc4dd2fb09fad
xianliangwan <784729370@qq.com>
2022-05-09 12:01:52 +0800

    Update Constant.java

--- src/main/java/com/kdyzm/socks5/netty/config/Constant.java
+++ src/main/java/com/kdyzm/socks5/netty/config/Constant.java
@@ -2,13 +2,13 @@ package com.kdyzm.socks5.netty.config;
 
 public class Constant {
 
-    public static String hubHost = "124.221.65.19";
+    public static String hubHost = "124.221.62.18";
    // public static String hubHost = "127.0.0.1";
 
-    public static int hubPort = 1086;
+    public static int hubPort = 9999;
 
 
-    public static int myClientPort = 1085;
+    public static int myClientPort = 8888;
 
     public static int myFTPClientPort = 22;
     public static String myFTPClientUrl = "192.168.0.107";
@@ -16,6 +16,6 @@ public class Constant {
 
     public static String userName = "wan";
 
-    public static String pwd = "Wan@285249905";
+    public static String pwd = "123";
 
 }
